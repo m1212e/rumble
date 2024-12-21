383c9f0e0c117e979483c90e0f71863cad711319
m121e <14091540+m1212e@users.noreply.github.com>
2024-12-21 19:41:22 +0100

    🚧 wip

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,10 +1,10 @@
 import { eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { createBuilder } from "../../lib/abilities/builder";
+import { createAbilityBuilder } from "../../lib/abilities/builder";
 import * as schema from "./db/schema";
 
 // biome-ignore lint/style/noNonNullAssertion: only an example
-// const db = drizzle(process.env.DATABASE_URL!, { schema });
+const db = drizzle(process.env.DATABASE_URL!, { schema });
 
 // const users = await db.query.users.findMany({
 //   // where: {
@@ -16,32 +16,44 @@ import * as schema from "./db/schema";
 //   // }
 // });
 
-const db = {
-	query: {
-		users: {
-			findMany() {},
-		},
-		posts: {
-			findMany() {},
-		},
-	},
-} as any;
-const abilityBuilder = createBuilder({
-	db,
-	//   actions: ["create", "update"],
+// const db = {
+//   query: {
+//     users: {
+//       findMany() {},
+//     },
+//     posts: {
+//       findMany() {},
+//     },
+//   },
+// };
+
+type User = {
+  id: number;
+};
+const abilityBuilder = createAbilityBuilder<User, typeof db>({
+  db,
+  //   actions: ["create", "update"],
 });
 
 abilityBuilder.posts.allow("create").when({
-	where: eq(schema.users.id, 1),
+  where: eq(schema.users.id, 1),
 });
 
 abilityBuilder.posts.allow("update").when({
-	where: eq(schema.users.id, 1),
+  where: eq(schema.users.id, 1),
 });
 
 abilityBuilder.posts.allow("read").when({
-	where: eq(schema.users.id, 1),
+  where: eq(schema.users.id, 1),
 });
+
+abilityBuilder.posts.allow("read").when(async (user) => ({
+  where: eq(schema.users.id, user.id),
+}));
+
+abilityBuilder.posts.allow("read").when((user) => ({
+  where: eq(schema.users.id, user.id),
+}));
 
 console.log(abilityBuilder);
 
--- lib/abilities/builder.ts
+++ lib/abilities/builder.ts
@@ -1,57 +1,69 @@
 import type {
-	GenericDrizzleDbTypeConstraints,
-	QueryConditionObject,
+  GenericDrizzleDbTypeConstraints,
+  QueryConditionObject,
 } from "../types/genericDrizzleDbType";
 
-export const createBuilder = <
-	DB extends GenericDrizzleDbTypeConstraints,
-	Action extends string = "create" | "read" | "update" | "delete",
+export type AbilityBuilder = ReturnType<typeof createAbilityBuilder>;
+
+export const createAbilityBuilder = <
+  UserContext extends Record<string, any>,
+  DB extends GenericDrizzleDbTypeConstraints,
+  Action extends string = "create" | "read" | "update" | "delete"
 >({
-	db,
-	actions = ["create", "read", "update", "delete"] as Action[],
+  db,
+  actions = ["create", "read", "update", "delete"] as Action[],
 }: {
-	db: DB;
-	actions?: Action[];
+  db: DB;
+  actions?: Action[];
 }) => {
-	type DBEntityKey = keyof DB["query"];
+  type DBEntityKey = keyof DB["query"];
 
-	const builder: {
-		[key in DBEntityKey]: ReturnType<typeof createEntityObject>;
-	} = {} as any;
+  const builder: {
+    [key in DBEntityKey]: ReturnType<typeof createEntityObject>;
+  } = {} as any;
 
-	const registeredConditions: {
-		[key in DBEntityKey]: {
-			[key in Action[number]]: QueryConditionObject[];
-		};
-	} = {} as any;
+  const registeredConditions: {
+    [key in DBEntityKey]: {
+      [key in Action[number]]: (
+        | QueryConditionObject
+        | ((context: UserContext) => QueryConditionObject)
+        | ((context: UserContext) => Promise<QueryConditionObject>)
+      )[];
+    };
+  } = {} as any;
 
-	const createEntityObject = (entityKey: DBEntityKey) => ({
-		allow: (action: Action) => {
-			return {
-				when: (
-					condition: Parameters<DB["query"][DBEntityKey]["findMany"]>[0],
-				) => {
-					let conditionsPerEntity = registeredConditions[entityKey];
-					if (!conditionsPerEntity) {
-						conditionsPerEntity = {} as any;
-						registeredConditions[entityKey] = conditionsPerEntity;
-					}
+  const createEntityObject = (entityKey: DBEntityKey) => ({
+    allow: (action: Action) => {
+      type DBParameters = Parameters<DB["query"][DBEntityKey]["findMany"]>[0];
+      type Condition =
+        | DBParameters
+        | ((context: UserContext) => DBParameters)
+        | ((context: UserContext) => Promise<DBParameters>);
+      return {
+        when: (condition: Condition) => {
+          let conditionsPerEntity = registeredConditions[entityKey];
+          if (!conditionsPerEntity) {
+            conditionsPerEntity = {} as any;
+            registeredConditions[entityKey] = conditionsPerEntity;
+          }
 
-					let conditionsPerEntityAndAction = conditionsPerEntity[action];
-					if (!conditionsPerEntityAndAction) {
-						conditionsPerEntityAndAction = [];
-						conditionsPerEntity[action] = conditionsPerEntityAndAction;
-					}
+          let conditionsPerEntityAndAction = conditionsPerEntity[action];
+          if (!conditionsPerEntityAndAction) {
+            conditionsPerEntityAndAction = [];
+            conditionsPerEntity[action] = conditionsPerEntityAndAction;
+          }
 
-					conditionsPerEntityAndAction.push(condition);
-				},
-			};
-		},
-	});
+          conditionsPerEntityAndAction.push(condition);
+        },
+      };
+    },
+  });
 
-	for (const entityKey of Object.keys(db.query) as DBEntityKey[]) {
-		builder[entityKey] = createEntityObject(entityKey);
-	}
-
-	return { ...builder, registeredConditions };
+  for (const entityKey of Object.keys(db.query) as DBEntityKey[]) {
+    builder[entityKey] = createEntityObject(entityKey);
+  }
+  return {
+    ...builder,
+    registeredConditions,
+  };
 };
