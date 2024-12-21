90863dd385a1d0bac4765da749d081986c31de31
m121e <14091540+m1212e@users.noreply.github.com>
2024-12-21 19:41:32 +0100

    🧼 format & lint

--- example/src/main.ts
+++ example/src/main.ts
@@ -28,31 +28,31 @@ const db = drizzle(process.env.DATABASE_URL!, { schema });
 // };
 
 type User = {
-  id: number;
+	id: number;
 };
 const abilityBuilder = createAbilityBuilder<User, typeof db>({
-  db,
-  //   actions: ["create", "update"],
+	db,
+	//   actions: ["create", "update"],
 });
 
 abilityBuilder.posts.allow("create").when({
-  where: eq(schema.users.id, 1),
+	where: eq(schema.users.id, 1),
 });
 
 abilityBuilder.posts.allow("update").when({
-  where: eq(schema.users.id, 1),
+	where: eq(schema.users.id, 1),
 });
 
 abilityBuilder.posts.allow("read").when({
-  where: eq(schema.users.id, 1),
+	where: eq(schema.users.id, 1),
 });
 
 abilityBuilder.posts.allow("read").when(async (user) => ({
-  where: eq(schema.users.id, user.id),
+	where: eq(schema.users.id, user.id),
 }));
 
 abilityBuilder.posts.allow("read").when((user) => ({
-  where: eq(schema.users.id, user.id),
+	where: eq(schema.users.id, user.id),
 }));
 
 console.log(abilityBuilder);
--- lib/gql/builder.ts
+++ lib/gql/builder.ts
@@ -1,58 +1,58 @@
 import SchemaBuilder from "@pothos/core";
-import type { GenericDrizzleDbTypeConstraints } from "../types/genericDrizzleDbType";
-import { createYoga, type YogaServerOptions } from "graphql-yoga";
+import { type YogaServerOptions, createYoga } from "graphql-yoga";
 import type { AbilityBuilder } from "../abilities/builder";
+import type { GenericDrizzleDbTypeConstraints } from "../types/genericDrizzleDbType";
 
 export const createGQLServer = async <
-  UserContext extends Record<string, any>,
-  DB extends GenericDrizzleDbTypeConstraints,
-  AbilityBuilderT extends AbilityBuilder,
-  RequestEvent extends Record<string, any>
+	UserContext extends Record<string, any>,
+	DB extends GenericDrizzleDbTypeConstraints,
+	AbilityBuilderT extends AbilityBuilder,
+	RequestEvent extends Record<string, any>,
 >({
-  db,
-  nativeServerOptions,
-  abilityBuilder,
-  context: makeUserContext,
+	db,
+	nativeServerOptions,
+	abilityBuilder,
+	context: makeUserContext,
 }: {
-  db: DB;
-  nativeServerOptions: Omit<
-    YogaServerOptions<RequestEvent, any>,
-    "schema" | "context"
-  >;
-  abilityBuilder: AbilityBuilderT;
-  context: (event: RequestEvent) => Promise<UserContext> | UserContext;
+	db: DB;
+	nativeServerOptions: Omit<
+		YogaServerOptions<RequestEvent, any>,
+		"schema" | "context"
+	>;
+	abilityBuilder: AbilityBuilderT;
+	context: (event: RequestEvent) => Promise<UserContext> | UserContext;
 }) => {
-  const nativeBuilder = new SchemaBuilder<{
-    // Context: Awaited<ReturnType<typeof combinedContext>>;
-    // Scalars: Scalars<Prisma.Decimal, Prisma.InputJsonValue | null, Prisma.InputJsonValue> & {
-    // 	File: {
-    // 		Input: File;
-    // 		Output: never;
-    // 	};
-    // 	JSONObject: {
-    // 		Input: any;
-    // 		Output: any;
-    // 	};
-    // };
-    DefaultFieldNullability: false;
-    DefaultArgumentNullability: false;
-    DefaultInputFieldRequiredness: true;
-  }>({
-    defaultFieldNullability: false,
-    defaultInputFieldRequiredness: true,
-  });
+	const nativeBuilder = new SchemaBuilder<{
+		// Context: Awaited<ReturnType<typeof combinedContext>>;
+		// Scalars: Scalars<Prisma.Decimal, Prisma.InputJsonValue | null, Prisma.InputJsonValue> & {
+		// 	File: {
+		// 		Input: File;
+		// 		Output: never;
+		// 	};
+		// 	JSONObject: {
+		// 		Input: any;
+		// 		Output: any;
+		// 	};
+		// };
+		DefaultFieldNullability: false;
+		DefaultArgumentNullability: false;
+		DefaultInputFieldRequiredness: true;
+	}>({
+		defaultFieldNullability: false,
+		defaultInputFieldRequiredness: true,
+	});
 
-  const nativeServer = createYoga<RequestEvent>({
-    ...nativeServerOptions,
-    schema: nativeBuilder.toSchema(),
-    context: (req) => {
-      const userContext = makeUserContext(req);
-      return userContext;
-    },
-  });
+	const nativeServer = createYoga<RequestEvent>({
+		...nativeServerOptions,
+		schema: nativeBuilder.toSchema(),
+		context: (req) => {
+			const userContext = makeUserContext(req);
+			return userContext;
+		},
+	});
 
-  return {
-    schemaBuilder: nativeBuilder,
-    server: nativeServer,
-  };
+	return {
+		schemaBuilder: nativeBuilder,
+		server: nativeServer,
+	};
 };
