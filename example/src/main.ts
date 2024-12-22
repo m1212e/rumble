7336c0edc2b6745da953ba152e62a50f9dfa425e
m121e <14091540+m1212e@users.noreply.github.com>
2024-12-22 14:16:48 +0100

    🧼 format & lint:

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,34 +1,34 @@
 import { eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { createAbilityBuilder } from "../../lib/abilities/builder";
-import * as schema from "./db/schema";
 import { createGQLServer } from "../../lib/gql/builder";
+import * as schema from "./db/schema";
 
 // biome-ignore lint/style/noNonNullAssertion: only an example
 const db = drizzle(process.env.DATABASE_URL!, { schema });
 
 type Context = {
-  userId: number;
+	userId: number;
 };
 const abilityBuilder = createAbilityBuilder<Context, typeof db>({
-  db,
+	db,
 });
 
 abilityBuilder.posts.allow("read");
 
 abilityBuilder.posts.allow("update").when((user) => ({
-  where: eq(schema.users.id, user.userId),
+	where: eq(schema.users.id, user.userId),
 }));
 
 const { schemaBuilder, server } = await createGQLServer({
-  abilityBuilder,
-  db,
+	abilityBuilder,
+	db,
 });
 
-schemaBuilder.drizzleObject('awdad', {
-	name: 'User',
+schemaBuilder.drizzleObject("awdad", {
+	name: "User",
 	fields: (t) => ({
-	  firstName: t.exposeString('first_name'),
-	  lastName: t.exposeString('last_name'),
+		firstName: t.exposeString("first_name"),
+		lastName: t.exposeString("last_name"),
 	}),
-  });
+});
