9477ef53e425b6ceb60b6ff6e307baa8b8166c32
m1212e <14091540+m1212e@users.noreply.github.com>
2025-04-18 12:43:14 +0200

    🚧 wip: pubsub pk strong typings

--- lib/pubsub.ts
+++ lib/pubsub.ts
@@ -1,4 +1,4 @@
-import type SchemaBuilder from "@pothos/core";
+import type { GetColumnData } from "drizzle-orm";
 import { createPubSub } from "graphql-yoga";
 import type { GenericDrizzleDbTypeConstraints } from "./types/genericDrizzleDbType";
 import type {
@@ -12,36 +12,6 @@ const SUBSCRIPTION_NOTIFIER_RUMBLE_PREFIX = "RUMBLE_SUBSCRIPTION_NOTIFICATION";
 const SUBSCRIPTION_NOTIFIER_REMOVED = "REMOVED";
 const SUBSCRIPTION_NOTIFIER_UPDATED = "UPDATED";
 const SUBSCRIPTION_NOTIFIER_CREATED = "CREATED";
-
-function makePubSubKey({
-	action,
-	tableName,
-	primaryKeyValue,
-}: {
-	tableName: string;
-	action: PubSubAction;
-	primaryKeyValue?: string;
-}) {
-	let actionKey: string;
-
-	switch (action) {
-		case "created":
-			actionKey = SUBSCRIPTION_NOTIFIER_CREATED;
-			break;
-		case "removed":
-			actionKey = SUBSCRIPTION_NOTIFIER_REMOVED;
-			break;
-		case "updated":
-			actionKey = SUBSCRIPTION_NOTIFIER_UPDATED;
-			break;
-		default:
-			throw new Error(`Unknown action: ${action}`);
-	}
-
-	return `${SUBSCRIPTION_NOTIFIER_RUMBLE_PREFIX}/${tableName}${
-		primaryKeyValue ? `/${primaryKeyValue}` : ""
-	}/${actionKey}`;
-}
 
 export type MakePubSubInstanceType<
 	UserContext extends Record<string, any>,
@@ -67,7 +37,8 @@ export const createPubSubInstance = <
 	PothosConfig extends CustomRumblePothosConfig,
 >({
 	subscriptions,
-}: RumbleInput<UserContext, DB, RequestEvent, Action, PothosConfig> & {}) => {
+	db,
+}: RumbleInput<UserContext, DB, RequestEvent, Action, PothosConfig>) => {
 	const pubsub = subscriptions
 		? createPubSub(...subscriptions)
 		: createPubSub();
@@ -78,59 +49,96 @@ export const createPubSubInstance = <
 		tableName,
 	}: {
 		tableName: ExplicitTableName;
-	}) => ({
-		/**
-		 * Call this when you want to register a subscription on an instance to this table
-		 */
-		registerOnInstance({
-			instance,
+	}) => {
+		type TablePrimaryKeyColumn = NonNullable<
+			DB["_"]["schema"]
+		>[ExplicitTableName]["primaryKey"][number];
+		type PrimaryKeyType = GetColumnData<TablePrimaryKeyColumn, "raw">;
+
+		function makePubSubKey({
 			action,
+			tableName,
 			primaryKeyValue,
 		}: {
-			instance: { register: (id: string) => void };
+			tableName: string;
 			action: PubSubAction;
-			primaryKeyValue?: string;
+			primaryKeyValue?: PrimaryKeyType;
 		}) {
-			const key = makePubSubKey({
-				tableName: tableName.toString(),
+			let actionKey: string;
+
+			switch (action) {
+				case "created":
+					actionKey = SUBSCRIPTION_NOTIFIER_CREATED;
+					break;
+				case "removed":
+					actionKey = SUBSCRIPTION_NOTIFIER_REMOVED;
+					break;
+				case "updated":
+					actionKey = SUBSCRIPTION_NOTIFIER_UPDATED;
+					break;
+				default:
+					throw new Error(`Unknown action: ${action}`);
+			}
+
+			return `${SUBSCRIPTION_NOTIFIER_RUMBLE_PREFIX}/${tableName}${
+				primaryKeyValue ? `/${primaryKeyValue}` : ""
+			}/${actionKey}`;
+		}
+
+		return {
+			/**
+			 * Call this when you want to register a subscription on an instance to this table
+			 */
+			registerOnInstance({
+				instance,
 				action,
 				primaryKeyValue,
-			});
-			instance.register(key);
-		},
-		/**
-		 * Call this when you created an entity of this table
-		 */
-		created() {
-			const key = makePubSubKey({
-				tableName: tableName.toString(),
-				action: "created",
-			});
-			return pubsub.publish(key);
-		},
-		/**
-		 * Call this when you removed an entity of this table
-		 */
-		removed(primaryKeyValue?: any) {
-			const key = makePubSubKey({
-				tableName: tableName.toString(),
-				action: "removed",
-				// primaryKeyValue,
-			});
-			return pubsub.publish(key);
-		},
-		/**
-		 * Call this when you updated an entity of this table
-		 */
-		updated(primaryKeyValue?: any) {
-			const key = makePubSubKey({
-				tableName: tableName.toString(),
-				action: "updated",
-				primaryKeyValue,
-			});
-			return pubsub.publish(key);
-		},
-	});
+			}: {
+				instance: { register: (id: string) => void };
+				action: PubSubAction;
+				primaryKeyValue?: string;
+			}) {
+				const key = makePubSubKey({
+					tableName: tableName.toString(),
+					action,
+					primaryKeyValue,
+				});
+				instance.register(key);
+			},
+			/**
+			 * Call this when you created an entity of this table
+			 */
+			created() {
+				const key = makePubSubKey({
+					tableName: tableName.toString(),
+					action: "created",
+				});
+				return pubsub.publish(key);
+			},
+			/**
+			 * Call this when you removed an entity of this table
+			 */
+			removed(primaryKeyValue?: PrimaryKeyType) {
+				const key = makePubSubKey({
+					tableName: tableName.toString(),
+					action: "removed",
+					// primaryKeyValue,
+				});
+				return pubsub.publish(key);
+			},
+			/**
+			 * Call this when you updated an entity of this table
+			 */
+			updated(primaryKeyValue?: PrimaryKeyType) {
+				const key = makePubSubKey({
+					tableName: tableName.toString(),
+					action: "updated",
+					primaryKeyValue,
+				});
+				return pubsub.publish(key);
+			},
+		};
+	};
 
 	return {
 		pubsub,
